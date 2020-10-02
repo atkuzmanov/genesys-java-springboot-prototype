7df58a3900e8524addcda0a80b2268053e878c40
atkuzmanov <atkuzmanov@gmail.com>
2020-10-02 13:58:39 +0300

    Work-In-Progress-WIP-2020-10-02-135839

---
+++ src/main/java/com/atkuzmanov/genesys/GenesysApplication.java
@@ -0,0 +1,13 @@
+package com.atkuzmanov.genesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesysApplication.class, args);
+	}
+
+}
