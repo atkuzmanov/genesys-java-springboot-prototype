136c51a4189acbf073684742a85fbd9a483b87ca
atkuzmanov <atkuzmanov@gmail.com>
2020-10-03 15:02:20 +0300

    Work-In-Progress-WIP-2020-10-03-150220

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # GeneSys
 
 ---
+
+## Setup
+
+- Setting up the Maven wrapper
+
+```sh
+mvn -N io.takari:maven:wrapper
+```
+
+---
--- src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
+++ src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class DefaultController {
 
     @RequestMapping("/")
     public String index() {
+        return localDateTimeNowFormat_yyyyMMdddHHmmss();
+    }
+
+    private static String localDateTimeNowFormat_yyyyMMdddHHmmss() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime localDateTime = LocalDateTime.now();
-        return localDateTime.toString();
+        return localDateTime.format(formatter);
     }
 }
