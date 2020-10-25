7bfa47dc9aa7a88ddd040141558f2769b4361f56
atkuzmanov <atkuzmanov@gmail.com>
2020-10-26 00:04:13 +0200

    Work-In-Progress-WIP-2020-10-26-000413

--- src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
+++ src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
@@ -1,7 +1,9 @@
 package com.atkuzmanov.genesys.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atkuzmanov.genesys.dao.TimestampEntity;
+import com.atkuzmanov.genesys.dao.TimestampRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,9 +11,29 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class DefaultController {
 
+    @Autowired
+    private TimestampRepository timestampRepo;
+
     @RequestMapping("/")
     public String index() {
         return localDateTimeNowFormat_yyyyMMdddHHmmss();
+    }
+
+    @PostMapping(path="/addTimestamp")
+    public @ResponseBody String addTimestampToDB(@RequestParam String newTimestamp){
+        TimestampEntity tse = new TimestampEntity();
+        if(newTimestamp.isBlank()) {
+            newTimestamp = localDateTimeNowFormat_yyyyMMdddHHmmss();
+        }
+        tse.setTimestampAsString(newTimestamp);
+        timestampRepo.save(tse);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<TimestampEntity> getAllTimestamps() {
+        // This returns a JSON or XML with all the timestamps.
+        return timestampRepo.findAll();
     }
 
     private static String localDateTimeNowFormat_yyyyMMdddHHmmss() {
