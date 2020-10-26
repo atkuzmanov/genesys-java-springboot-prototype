e767a230b9830a79ecee55366e49686492e392db
atkuzmanov <atkuzmanov@gmail.com>
2020-10-26 09:56:03 +0200

    Work-In-Progress-WIP-2020-10-26-095603

--- src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
+++ src/main/java/com/atkuzmanov/genesys/controllers/DefaultController.java
@@ -5,8 +5,10 @@ import com.atkuzmanov.genesys.dao.TimestampRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.InvalidParameterException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @RestController
 public class DefaultController {
@@ -19,21 +21,49 @@ public class DefaultController {
         return localDateTimeNowFormat_yyyyMMdddHHmmss();
     }
 
-    @PostMapping(path="/addTimestamp")
-    public @ResponseBody String addTimestampToDB(@RequestParam String newTimestamp){
+    @PostMapping(path = "/addTimestamp")
+    public @ResponseBody
+    String addTimestampToDB(@RequestParam String newTimestamp) {
         TimestampEntity tse = new TimestampEntity();
-        if(newTimestamp.isBlank()) {
+        if (newTimestamp.isBlank()) {
             newTimestamp = localDateTimeNowFormat_yyyyMMdddHHmmss();
         }
         tse.setTimestampAsString(newTimestamp);
         timestampRepo.save(tse);
-        return "Saved";
+        return "Saved.";
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<TimestampEntity> getAllTimestamps() {
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    Iterable<TimestampEntity> getAllTimestamps() {
         // This returns a JSON or XML with all the timestamps.
         return timestampRepo.findAll();
+    }
+
+    @PutMapping(path="/updateTimestamp")
+    public @ResponseBody
+    String updateTimestamp(@RequestParam int timestampId) {
+        if (timestampId <= 0) {
+            throw new InvalidParameterException("Timestamp id must be a positive integer.");
+        }
+        Optional<TimestampEntity> optTse = timestampRepo.findById(timestampId);
+        if (!optTse.isPresent()) {
+            return "Not found.";
+        }
+        TimestampEntity tse = optTse.get();
+        tse.setTimestampAsString(localDateTimeNowFormat_yyyyMMdddHHmmss());
+        timestampRepo.save(tse);
+        return "Updated.";
+    }
+
+    @DeleteMapping(path = "/delete")
+    public @ResponseBody
+    String deleteTimestamp(@RequestParam int timestampId) {
+        if (timestampId <= 0) {
+            throw new InvalidParameterException("Timestamp id must be a positive integer.");
+        }
+        timestampRepo.deleteById(timestampId);
+        return "Deleted.";
     }
 
     private static String localDateTimeNowFormat_yyyyMMdddHHmmss() {
