4b473367830b79e7151030b4b24a1ea633aa4a50
atkuzmanov <atkuzmanov@gmail.com>
2020-10-27 14:00:48 +0200

    AOP logging WIP 2020-10-27-1400 - disassembled, not working, work in progress... logging request...

--- src/main/java/com/atkuzmanov/genesys/aop/RestLoggingAspect.java
+++ src/main/java/com/atkuzmanov/genesys/aop/RestLoggingAspect.java
@@ -5,21 +5,18 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
 
 @Aspect
 @Component
@@ -39,65 +36,70 @@ public class RestLoggingAspect {
 
         System.out.println(">>> targetClass" + targetClass);
 
-//        requestDetails(joinPoint, targetClass);
-    }
-
-    private void requestDetails(JoinPoint joinPoint, Class targetClass) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-//        PostMapping methodPostMapping = method.getAnnotation(PostMapping.class);
-        GetMapping methodRequestMapping = method.getAnnotation(GetMapping.class);
-
-        RequestMapping requestMapping = (RequestMapping) targetClass.getAnnotation(RequestMapping.class);
-
-//        if (methodRequestMapping != null) {
-//            System.out.println(">>> methodRequestMapping.name(): " + methodRequestMapping.name());
-//            System.out.println(">>> methodRequestMapping.consumes() : " + Arrays.toString(methodRequestMapping.consumes()));
-//            System.out.println(">>> methodRequestMapping.headers()) : " + Arrays.toString(methodRequestMapping.headers()));
-////            System.out.println(">>> methodRequestMapping.method()) : " + methodRequestMapping.method());
-//            System.out.println(">>> methodRequestMapping.params()) : " + Arrays.toString(methodRequestMapping.params()));
-//            System.out.println(">>> methodRequestMapping.path()) : " + Arrays.toString(methodRequestMapping.path()));
-//            System.out.println(">>> methodRequestMapping.produces() : " + Arrays.toString(methodRequestMapping.produces()));
-//            System.out.println(">>> methodRequestMapping.value()) : " + Arrays.toString(methodRequestMapping.value()));
-//            System.out.println(">>> methodRequestMapping.toString() : " + methodRequestMapping.toString());
-//        }
-
-        if (requestMapping != null) {
-            System.out.println(">>> requestMapping.name(): " + requestMapping.name());
-            System.out.println(">>> requestMapping.consumes() : " + Arrays.toString(requestMapping.consumes()));
-            System.out.println(">>> requestMapping.headers()) : " + Arrays.toString(requestMapping.headers()));
-            System.out.println(">>> requestMapping.method()) : " + Arrays.toString(requestMapping.method()));
-            System.out.println(">>> requestMapping.params()) : " + Arrays.toString(requestMapping.params()));
-            System.out.println(">>> requestMapping.path()) : " + Arrays.toString(requestMapping.path()));
-            System.out.println(">>> requestMapping.produces() : " + Arrays.toString(requestMapping.produces()));
-            System.out.println(">>> requestMapping.value()) : " + Arrays.toString(requestMapping.value()));
-            System.out.println(">>> requestMapping.toString() : " + requestMapping.toString());
-        }
-
-//        Annotation[] methodAnnotations = method.getDeclaredAnnotations();
-//        for (Annotation an : methodAnnotations) {
-//            System.out.println(">>> annotation: " + an);
-//        }
-
-
-    }
-
-    private void extractRequestDetails(RequestMapping requestMapping, Annotation annotation) {
-
-    }
-
-    private String getUrl(String[] urls) {
-        if (urls.length == 0) return "";
-        else return urls[0];
-    }
-
-    @AfterReturning(pointcut = "execution(* com.atkuzmanov.genesys.controllers.*.*(..))", returning = "result")
-    public void auditInfo(JoinPoint joinPoint, Object result) {
-        System.out.println(">>><<< Here! 2");
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
 
-        System.out.println(request.getRequestURI());
+        System.out.println(">>> request.getRequestURI()): " + request.getRequestURI());
+        System.out.println(">>> request.getMethod()): " + request.getMethod());
+        System.out.println(">>> request.getScheme()): " + request.getScheme());
+        System.out.println(">>> request.getProtocol()): " + request.getProtocol());
+        System.out.println(">>> request.getLocalName()): " + request.getLocalName());
+        System.out.println(">>> request.getLocalAddr()): " + request.getLocalAddr());
+        System.out.println(">>> request.getLocale())): " + request.getLocale());
+        System.out.println(">>> request.getLocales())): " + request.getLocales());
+        System.out.println(">>> request.getQueryString()): " + request.getQueryString());
+        System.out.println(">>> request.getLocalName()): " + request.getLocalName());
+        System.out.println(">>> request.getServletPath()): " + request.getServletPath());
+        System.out.println(">>> request.getRemoteHost()): " + request.getRemoteHost());
+        System.out.println(">>> request.getServerName()): " + request.getServerName());
+        System.out.println(">>> request.getServerPort()): " + request.getServerPort());
+
+
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String paramName = params.nextElement();
+            System.out.println("Parameter Name - " + paramName + ", Value - " + request.getParameter(paramName));
+        }
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                System.out.println(headerName + " : " + request.getHeader(headerName));
+            }
+        }
+
+        StringBuilder buffer = new StringBuilder();
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append(System.lineSeparator());
+            }
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        String payload = buffer.toString();
+        System.out.println(">>> payload : " + payload);
+
+//            BufferedReader reader = request.getReader();
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            buffer.append(line);
+//            buffer.append(System.lineSeparator());
+//        }
+//        String data = buffer.toString();
+    }
+
+
+    @AfterReturning(pointcut = "execution(* com.atkuzmanov.genesys.controllers.*.*(..))", returning = "result")
+    public void auditInfo(JoinPoint joinPoint, Object result) {
+//        System.out.println(">>><<< Here! 2");
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+
+//        System.out.println(request.getRequestURI());
 
         if (result instanceof ResponseEntity) {
             ResponseEntity responseObj = (ResponseEntity) result;
@@ -111,7 +113,7 @@ public class RestLoggingAspect {
             int httpResponseStatus = responseObj.getStatusCode().value();
 //            responseObj.getEntity();
 // Can log whatever stuff from here in a single spot.
-            System.out.println(">>> " + requestUrl);
+//            System.out.println(">>> " + requestUrl);
         }
     }
 }
