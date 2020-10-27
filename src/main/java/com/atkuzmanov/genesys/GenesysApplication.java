dba3ac10b6efaed690aebf67235f1edc4e512e3c
atkuzmanov <atkuzmanov@gmail.com>
2020-10-27 17:59:55 +0200

    Work-In-Progress-WIP-2020-10-27-175955

--- src/main/java/com/atkuzmanov/genesys/GenesysApplication.java
+++ src/main/java/com/atkuzmanov/genesys/GenesysApplication.java
@@ -1,5 +1,7 @@
 package com.atkuzmanov.genesys;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,8 +10,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class GenesysApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(GenesysApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GenesysApplication.class, args);
+		log.info("<<<>>> Application started.");
 	}
 
 }
