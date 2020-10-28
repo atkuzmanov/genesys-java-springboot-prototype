10de7abc0dbbd0f24e1fe43ae62adeab6637cf23
atkuzmanov <atkuzmanov@gmail.com>
2020-10-28 18:33:12 +0200

    Work-In-Progress-WIP-2020-10-28-183312

--- src/main/java/com/atkuzmanov/genesys/aop/RestLoggingAspect.java
+++ src/main/java/com/atkuzmanov/genesys/aop/RestLoggingAspect.java
@@ -16,8 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Enumeration;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.lang.String.valueOf;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+import static net.logstash.logback.argument.StructuredArguments.entries;
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @Aspect
@@ -33,36 +38,71 @@ public class RestLoggingAspect {
     // TODO: wip
     @Before("requestPointcut()")
     public void logRequest(JoinPoint joinPoint) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         Class<?> targetClass = joinPoint.getTarget().getClass();
-        Logger requestLogger = LoggerFactory.getLogger(targetClass);
-        requestLogger.info("<<< request logger!!",
-                kv("method", joinPoint.getSignature().getName()),
-                kv("class", targetClass));
+        Logger requestLog = LoggerFactory.getLogger(targetClass);
+//        requestLog.info("INCOMING_REQUEST",
+//                kv("uri", request.getRequestURI()),
+//                kv("url", request.getRequestURL()),
+//                kv("requestMethod", request.getMethod()),
+//                kv("requestScheme", request.getScheme()),
+//                kv("request Protocol", request.getProtocol()),
+//                kv("request LocalName", request.getLocalName()),
+//                kv("request LocalAddress", request.getLocalAddr()),
+//                kv("request Locale", request.getLocale()),
+//                kv("request Locales", request.getLocales().toString()),
+//                kv("method", joinPoint.getSignature().getName()),
+//                kv("class", targetClass)
+//
+//        );
 
-        System.out.println(">>> targetClass" + targetClass);
 
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-                .getRequest();
+        Map<String, String> requestLogMap = new HashMap();
+        if (nonNull(request)) {
+            requestLogMap.put("uri", request.getRequestURI());
+            requestLogMap.put("url", request.getRequestURL().toString());
+            requestLogMap.put("path", request.getServletPath());
+            requestLogMap.put("requestMethod", request.getMethod());
+            requestLogMap.put("requestScheme", request.getScheme());
+            requestLogMap.put("request Protocol", request.getProtocol());
+            requestLogMap.put("request LocalName", request.getLocalName());
+            requestLogMap.put("request Locale", request.getLocale().toString());
+            requestLogMap.put("request Locales", Collections.list(request.getLocales()).toString());
+            requestLogMap.put("request QueryString", request.getQueryString());
+            requestLogMap.put("request RemoteHost ", request.getRemoteHost());
+            requestLogMap.put("request ServerName ", request.getServerName());
+            requestLogMap.put("request ServerPort ", String.valueOf(request.getServerPort()));
 
-        System.out.println(">>> request.getRequestURI()): " + request.getRequestURI());
-        System.out.println(">>> request.getMethod()): " + request.getMethod());
-        System.out.println(">>> request.getScheme()): " + request.getScheme());
-        System.out.println(">>> request.getProtocol()): " + request.getProtocol());
-        System.out.println(">>> request.getLocalName()): " + request.getLocalName());
-        System.out.println(">>> request.getLocalAddr()): " + request.getLocalAddr());
-        System.out.println(">>> request.getLocale())): " + request.getLocale());
-        System.out.println(">>> request.getLocales())): " + request.getLocales());
-        System.out.println(">>> request.getQueryString()): " + request.getQueryString());
-        System.out.println(">>> request.getLocalName()): " + request.getLocalName());
-        System.out.println(">>> request.getServletPath()): " + request.getServletPath());
-        System.out.println(">>> request.getRemoteHost()): " + request.getRemoteHost());
-        System.out.println(">>> request.getServerName()): " + request.getServerName());
-        System.out.println(">>> request.getServerPort()): " + request.getServerPort());
+            requestLogMap.put("method", joinPoint.getSignature().getName());
+            requestLogMap.put("class", targetClass.toString());
+            requestLogMap.values().removeIf(value -> value == null || value.trim().length() == 0);
+        }
 
-        logger.info("<<< request.getRequestURI()): " + request.getRequestURI());
-        logger.info("{}", request.getLocalAddr());
-        logger.info("Hello, World JSON!");
-        logger.debug("Hello World JSON.");
+//        requestLogMap.keySet().removeAll(
+//                requestLogMap.entrySet().stream()
+//                        .filter(a -> a.getValue().isBlank())
+//                        .map(e -> e.getKey()).collect(Collectors.toList()));
+
+
+        requestLog.info("<<< INCOMING_REQUEST", entries(requestLogMap));
+
+//        System.out.println(">>> request.getRequestURI()): " + request.getRequestURI());
+//        System.out.println(">>> request.getMethod()): " + request.getMethod());
+//        System.out.println(">>> request.getScheme()): " + request.getScheme());
+//        System.out.println(">>> request.getProtocol()): " + request.getProtocol());
+//        System.out.println(">>> request.getLocalName()): " + request.getLocalName());
+//        System.out.println(">>> request.getLocalAddr()): " + request.getLocalAddr());
+//        System.out.println(">>> request.getLocale())): " + request.getLocale());
+//        System.out.println(">>> request.getLocales())): " + request.getLocales());
+//        System.out.println(">>> request.getQueryString()): " + request.getQueryString());
+//        System.out.println(">>> request.getServletPath()): " + request.getServletPath());
+//        System.out.println(">>> request.getRemoteHost()): " + request.getRemoteHost());
+//        System.out.println(">>> request.getServerName()): " + request.getServerName());
+//        System.out.println(">>> request.getServerPort()): " + request.getServerPort());
+//        logger.info("<<< request.getRequestURI()): " + request.getRequestURI());
+//        logger.info("{}", request.getLocalAddr());
+//        logger.info("Hello, World JSON!");
+//        logger.debug("Hello World JSON.");
 
         Enumeration<String> params = request.getParameterNames();
         while (params.hasMoreElements()) {
