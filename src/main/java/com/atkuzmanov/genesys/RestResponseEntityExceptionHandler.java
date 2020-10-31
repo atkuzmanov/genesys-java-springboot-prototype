76b5adff5e721c886a8786c8cf7e2012d39322d9
atkuzmanov <atkuzmanov@gmail.com>
2020-10-31 14:22:50 +0200

    Work-In-Progress-WIP-2020-10-31-142250

--- src/main/java/com/atkuzmanov/genesys/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/atkuzmanov/genesys/RestResponseEntityExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.atkuzmanov.genesys;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.ConversionFailedException;
@@ -14,7 +12,6 @@ import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.lang.reflect.InvocationTargetException;
@@ -22,12 +19,11 @@ import java.lang.reflect.InvocationTargetException;
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-//    private static final Logger log = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
 
     /**
      * Catch all for any other exceptions...
      */
-    @ExceptionHandler({ Exception.class })
+    @ExceptionHandler({Exception.class})
     @ResponseBody
     public ResponseEntity<?> handleAnyException(Exception e) {
         return errorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -36,37 +32,32 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     /**
      * Handle failures commonly thrown from code
      */
-    @ExceptionHandler({ InvocationTargetException.class, IllegalArgumentException.class, ClassCastException.class,
-            ConversionFailedException.class })
+    @ExceptionHandler({InvocationTargetException.class, IllegalArgumentException.class, ClassCastException.class,
+            ConversionFailedException.class})
     @ResponseBody
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity handleMiscFailures(Throwable t) {
+    public ResponseEntity<?> handleMiscFailures(Throwable t) {
         return errorResponse(t, HttpStatus.BAD_REQUEST);
     }
 
     /**
      * Send a 409 Conflict in case of concurrent modification
      */
-    @ExceptionHandler({ ObjectOptimisticLockingFailureException.class, OptimisticLockingFailureException.class,
-            DataIntegrityViolationException.class })
+    @ExceptionHandler({ObjectOptimisticLockingFailureException.class, OptimisticLockingFailureException.class,
+            DataIntegrityViolationException.class})
     @ResponseBody
-    public ResponseEntity handleConflict(Exception ex) {
+    public ResponseEntity<?> handleConflict(Exception ex) {
         return errorResponse(ex, HttpStatus.CONFLICT);
     }
 
-    protected ResponseEntity<Object> errorResponse(Throwable throwable,
-                                                             HttpStatus status) {
-        if (null != throwable) {
-//            log.error("error caught: " + throwable.getMessage(), throwable);
+    protected ResponseEntity<Object> errorResponse(Throwable throwable, HttpStatus status) {
+        if (throwable != null) {
             return response(new Exception(throwable), status);
         } else {
-//            log.error("unknown error caught in RESTController, {}", status);
             return response(null, status);
         }
     }
 
     protected <T> ResponseEntity<T> response(T body, HttpStatus status) {
-//        log.debug("Responding with a status of {}", status);
         return new ResponseEntity<>(body, new HttpHeaders(), status);
     }
 }
