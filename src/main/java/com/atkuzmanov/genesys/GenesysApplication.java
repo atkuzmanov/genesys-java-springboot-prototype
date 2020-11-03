65fe90268651291e9986584e1d9f9e611520dd18
atkuzmanov <atkuzmanov@gmail.com>
2020-11-03 18:52:11 +0200

    Work-In-Progress-WIP-2020-11-03-185211

--- src/main/java/com/atkuzmanov/genesys/GenesysApplication.java
+++ src/main/java/com/atkuzmanov/genesys/GenesysApplication.java
@@ -1,7 +1,5 @@
 package com.atkuzmanov.genesys;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
@@ -12,17 +10,16 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class GenesysApplication {
-    private static final Logger log = LoggerFactory.getLogger(GenesysApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GenesysApplication.class, args);
-        log.info("Application started.");
     }
 
     /**
      * Enables Spring Boot's actuator trace
      * http://localhost:8080/actuator/httptrace
      * See: https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-http-tracing
+     *
      * @return InMemoryHttpTraceRepository
      */
     @Bean
