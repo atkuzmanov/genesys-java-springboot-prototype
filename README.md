7a37e08e4cea959fe8d235b82983092afa48af49
atkuzmanov <atkuzmanov@gmail.com>
2020-11-07 15:35:11 +0200

    Work-In-Progress-WIP-2020-11-07-153510

--- README.md
+++ README.md
@@ -9,6 +9,35 @@
 ```sh
 mvn -N io.takari:maven:wrapper
 ```
+---
+
+## Notes
+
+### ASPECT LOGGING and DISTRIBUTED TRACING
+
+This [StackOverflow question/thread](https://stackoverflow.com/questions/33744875/spring-boot-how-to-log-all-requests-and-responses-with-exceptions-in-single-pl) 
+demonstrates how hard the problem of combining LOGGING with DISTRIBUTED TRACING is, especially when you are using ASPECT ORIENTED PROGRAMMING (AOP) with Java and Spring Boot.
+Furthermore this thread is only about logging all requests, things get really "fun", when you start adding DISTRIBUTED TRACING etc.
+
+While you have many individual options for just one or the other, the difficulty arises when you try to combine them and make them play nice.
+I am referring to combining the following:
+- AOP (Controller Advice, ResponseBodyAdvice etc.)
+- Logging (cross-cutting concern => AOP)
+- Filters (Request/Responce Filters etc.)
+- Interceptors (Request/Responce Interceptors etc.)
+- Handlers (Exception Handlers etc.)
+- Servlets
+- Spring/Spring Boot Configuration
+- Dependencies
+- Java version
+
+As you can see because of the amount of topics, and the amount of possible approaches for each topic, 
+the amount of possible successful combinations grows exponentially small. 
+It becomes almost as trying to find a needle in a haystack, as when you have just fixed or made two of them play nice 
+together, the third next thing you need for the next functionality breaks one of the previous two.
+
+> References:
+> <https://stackoverflow.com/questions/33744875/spring-boot-how-to-log-all-requests-and-responses-with-exceptions-in-single-pl>
 
 ---
 
--- src/main/java/com/atkuzmanov/genesys/rest/LoggingFilter.java
+++ src/main/java/com/atkuzmanov/genesys/rest/LoggingFilter.java
@@ -25,7 +25,12 @@ import java.util.List;
 public class LoggingFilter extends OncePerRequestFilter {
 
     private final LoggingService logServ = new LoggingService();
-    // TODO: Possible improvement - extract excluded urls to properties configuration for different profiles.
+    /* Here you can exclude urls, so the response does not get logged twice,
+     * once by doFilterInternal() and once by the Aspect.
+     * See:
+     * https://stackoverflow.com/questions/39212551/how-do-i-exclude-a-specific-url-in-a-filter-in-spring
+     * TODO: Possible improvement - extract excluded urls to properties configuration for different profiles.
+     */
     private static final List<String> EXCLUDE_URL = Arrays.asList("/health", "/httptrace", "/info", "/updateTimestamp");
 
     @Bean
@@ -42,16 +47,10 @@ public class LoggingFilter extends OncePerRequestFilter {
         return registrationBean;
     }
 
-
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        return EXCLUDE_URL.stream().anyMatch(exclude -> exclude.equalsIgnoreCase(request.getServletPath()));
-    }
-
     /**
      * Note: Regarding Distributed Tracing, Sleuth and Zipkin
      * The @traceId @spanId etc. are added when logging through the AOP Aspect.
-     * If the logging is not going through there, then they need to be manuallya added here.
+     * If the logging is not going through there, then they need to be manually added here.
      * See:
      * https://cloud.spring.io/spring-cloud-sleuth/2.0.x/single/spring-cloud-sleuth.html#__literal_tracingfilter_literal
      * https://stackoverflow.com/questions/41222405/adding-the-traceid-from-spring-cloud-sleuth-to-response
@@ -66,6 +65,9 @@ public class LoggingFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
         ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(httpServletResponse);
+
+        /* The line below is very important!
+         */
         filterChain.doFilter(requestWrapper, responseWrapper);
 
         logServ.logContentCachingResponse(responseWrapper, this.getClass().getName(), "doFilterInternal");
@@ -73,4 +75,10 @@ public class LoggingFilter extends OncePerRequestFilter {
         // The line below is very important!
         responseWrapper.copyBodyToResponse();
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return EXCLUDE_URL.stream().anyMatch(exclude -> exclude.equalsIgnoreCase(request.getServletPath()));
+    }
+
 }
